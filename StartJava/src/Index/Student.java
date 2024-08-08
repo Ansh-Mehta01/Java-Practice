6833ec052fc30f5f2e9c3167d8e7d05573988d4b
ansh <anshmehta434@gmail.com>
2024-08-08 12:27:46 +0530

    second commit

--- StartJava/src/Index/Student.java
+++ StartJava/src/Index/Student.java
@@ -1,5 +1,23 @@
 package Index;
 
+import first.Constructor;
+
 public class Student {
-       
-}
+	public Student() {
+		this(1,2,3);
+		System.out.println("Default Constructor");
+	}  
+	public Student(int a) {
+		this(1,2);
+		System.out.println("One Constructor");
+	}
+	public Student(int a , int b) {
+		this();
+		System.out.println("Two Constructor");
+	}
+	public Student(int a, int b , int c) {
+		System.out.println("three Constructor");
+	}
+	public static void main(String[] args) {
+		Student cd= new Student(3,4);
+	}}
